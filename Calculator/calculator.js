c5e101ae656eda19a20ff41c6da4e094512834e2
Jun99uu <igun0423@naver.com>
2022-03-25 02:13:50 +0900

    update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,145 +1,270 @@
 const calForm = document.querySelector("#cal-form");
-const calInput = document.querySelector("#cal-input");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
 const addBtn = document.querySelector("#add-btn");
 const minBtn = document.querySelector("#min-btn");
 const mulBtn = document.querySelector("#mul-btn");
 const divBtn = document.querySelector("#div-btn");
 const eqBtn = document.querySelector("#eq-btn");
 const preview = document.querySelector("#preview");
+const cancleBtn = document.querySelector("#cancle-btn");
 
 let process = "";
 
-let value;
-let sign;
-let result;
+let value = ""; //입력된 값
+let result = 0; //결과 값
+let sign = ""; //기호
+let inputNum = ""; //입력 숫자
 
-function handleAdd(e) {
-  if (calInput.value !== "") {
-    const num = Number(calInput.value);
-    if (!sign) {
-      value = num;
-      sign = "+";
+function handleOne(e) {
+  if (inputNum === "") {
+    inputNum = "1";
+  } else {
+    inputNum += "1";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleTwo(e) {
+  if (inputNum === "") {
+    inputNum = "2";
+  } else {
+    inputNum += "2";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleThree(e) {
+  if (inputNum === "") {
+    inputNum = "3";
+  } else {
+    inputNum += "3";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleFour(e) {
+  if (inputNum === "") {
+    inputNum = "4";
+  } else {
+    inputNum += "4";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleFive(e) {
+  if (inputNum === "") {
+    inputNum = "5";
+  } else {
+    inputNum += "5";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleSix(e) {
+  if (inputNum === "") {
+    inputNum = "6";
+  } else {
+    inputNum += "6";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleSeven(e) {
+  if (inputNum === "") {
+    inputNum = "7";
+  } else {
+    inputNum += "7";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleEight(e) {
+  if (inputNum === "") {
+    inputNum = "8";
+  } else {
+    inputNum += "8";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleNine(e) {
+  if (inputNum === "") {
+    inputNum = "9";
+  } else {
+    inputNum += "9";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleZero(e) {
+  if (inputNum === "") {
+    inputNum = "0";
+  } else {
+    inputNum += "0";
+  }
+  value = Number(inputNum);
+  preview.textContent = value;
+}
+
+function handleAdd() {
+  if (value !== "") {
+    //숫자가 입력 되었을 때
+    if (sign === "") {
+      //이전에 입력한 기호가 없을 때
+      result = value; //계산값에 이전 입력값 넣어줌
+      sign = "+"; //기호에 더하기 넣어줌
+      inputNum = "";
+      value = "";
     } else {
+      //이전에 입력한 기호가 있을 때
       if (sign === "+") {
-        value += num;
+        result = result + value;
       } else if (sign === "-") {
-        value -= num;
+        result -= value;
       } else if (sign === "*") {
-        value *= num;
+        result *= value;
       } else if (sign === "/") {
-        value /= num;
+        result /= value;
       }
       sign = "+";
+      inputNum = "";
+      value = "";
     }
-    calInput.value = "";
-    preview.textContent = value + sign;
+    preview.textContent = result;
   } else {
     alert("숫자 입력 안했자나욥👊");
   }
 }
 
 function handleMin(e) {
-  if (calInput.value !== "") {
-    const num = Number(calInput.value);
-    if (!sign) {
-      value = num;
-      sign = "-";
+  if (value !== "") {
+    //숫자가 입력 되었을 때
+    if (sign === "") {
+      //이전에 입력한 기호가 없을 때
+      result = value; //계산값에 이전 입력값 넣어줌
+      sign = "-"; //기호에 더하기 넣어줌
+      inputNum = "";
+      value = "";
     } else {
+      //이전에 입력한 기호가 있을 때
       if (sign === "+") {
-        value += num;
+        result += value;
       } else if (sign === "-") {
-        value -= num;
+        result -= value;
       } else if (sign === "*") {
-        value *= num;
+        result *= value;
       } else if (sign === "/") {
-        value /= num;
+        result /= value;
       }
       sign = "-";
+      inputNum = "";
+      value = "";
     }
-    calInput.value = "";
-    preview.textContent = value + sign;
+    preview.textContent = result;
   } else {
     alert("숫자 입력 안했자나욥👊");
   }
 }
 
 function handleMul(e) {
-  if (calInput.value !== "") {
-    const num = Number(calInput.value);
-    if (!sign) {
-      value = num;
-      sign = "*";
+  if (value !== "") {
+    //숫자가 입력 되었을 때
+    if (sign === "") {
+      //이전에 입력한 기호가 없을 때
+      result = value; //계산값에 이전 입력값 넣어줌
+      sign = "*"; //기호에 더하기 넣어줌
+      inputNum = "";
     } else {
+      //이전에 입력한 기호가 있을 때
       if (sign === "+") {
-        value += num;
+        result += value;
       } else if (sign === "-") {
-        value -= num;
+        result -= value;
       } else if (sign === "*") {
-        value *= num;
+        result *= value;
       } else if (sign === "/") {
-        value /= num;
+        result /= value;
       }
       sign = "*";
+      inputNum = "";
+      value = "";
     }
-    calInput.value = "";
-    preview.textContent = value + sign;
+    preview.textContent = result;
   } else {
     alert("숫자 입력 안했자나욥👊");
   }
 }
 
 function handleDiv(e) {
-  if (calInput.value !== "") {
-    const num = Number(calInput.value);
-    if (!sign) {
-      value = num;
-      sign = "/";
+  if (value !== "") {
+    //숫자가 입력 되었을 때
+    if (sign === "") {
+      //이전에 입력한 기호가 없을 때
+      result = value; //계산값에 이전 입력값 넣어줌
+      sign = "/"; //기호에 더하기 넣어줌
+      inputNum = "";
     } else {
+      //이전에 입력한 기호가 있을 때
       if (sign === "+") {
-        value += num;
+        result += value;
       } else if (sign === "-") {
-        value -= num;
+        result -= value;
       } else if (sign === "*") {
-        value *= num;
+        result *= value;
       } else if (sign === "/") {
-        value /= num;
+        result /= value;
       }
       sign = "/";
+      inputNum = "";
+      value = "";
     }
-    calInput.value = "";
-    preview.textContent = value + sign;
+    preview.textContent = result;
   } else {
     alert("숫자 입력 안했자나욥👊");
   }
 }
 
 function handleEq(e) {
-  if (calInput.value !== "") {
-    const num = Number(calInput.value);
-    if (!sign && value) {
-      calInput.value = "";
-      preview.textContent = `정답은 ${value}입니다🐸`;
-    } else if (sign) {
-      if (sign === "+") {
-        value += num;
-      } else if (sign === "-") {
-        value -= num;
-      } else if (sign === "*") {
-        value *= num;
-      } else if (sign === "/") {
-        value /= num;
-      }
-      calInput.value = "";
-      preview.textContent = `정답은 ${value}입니다🐸`;
-    } else if (!sign && !value && num) {
-      calInput.value = "";
-      preview.textContent = `정답은 ${num}입니다🐸`;
-    } else {
-      alert("숫자 입력 안했자나욥👊");
+  if (value !== "") {
+    if (sign === "+") {
+      result += value;
+    } else if (sign === "-") {
+      result -= value;
+    } else if (sign === "*") {
+      result *= value;
+    } else if (sign === "/") {
+      result /= value;
     }
-  } else {
-    alert("숫자 입력 안했자나욥👊");
   }
+  sign = "";
+  inputNum = "";
+  value = result;
+  preview.textContent = result + "🐸";
+}
+
+function handleCancle() {
+  value = "";
+  result = 0;
+  inputNum = "";
+  preview.textContent = "0";
 }
 
 function handleToDoSubmit(event) {
@@ -152,3 +277,14 @@ minBtn.addEventListener("click", handleMin);
 mulBtn.addEventListener("click", handleMul);
 divBtn.addEventListener("click", handleDiv);
 eqBtn.addEventListener("click", handleEq);
+one.addEventListener("click", handleOne);
+two.addEventListener("click", handleTwo);
+three.addEventListener("click", handleThree);
+four.addEventListener("click", handleFour);
+five.addEventListener("click", handleFive);
+six.addEventListener("click", handleSix);
+seven.addEventListener("click", handleSeven);
+eight.addEventListener("click", handleEight);
+nine.addEventListener("click", handleNine);
+zero.addEventListener("click", handleZero);
+cancleBtn.addEventListener("click", handleCancle);
